ed0608e840005dc4aa9eed334094d8b945f016f2
Joaquim Leitão <jocaleitao93@gmail.com>
2017-04-26 18:31:43 +0100

    Added run file

---
+++ TP3/run.R
@@ -0,0 +1,7 @@
+# Joaquim Leitão - 2011150072
+# 2016/2017 School Year
+# Doctoral Program in Information Science and Technology - Real Time Learning in Intelligent Systems
+# Assignment 3
+
+source('code/main.R')
+
